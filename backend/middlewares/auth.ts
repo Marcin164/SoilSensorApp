2e864541500cfaec2824386929d6336115c96616
Marcin <1999nowakowskimarcin@gmail.com>
2023-04-10 21:52:34 +0200

    init and configure frontend and backend

--- backend/middlewares/auth.ts
+++ backend/middlewares/auth.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import JwtExpired from 'jwt-check-expiration'
 
-const auth = (req, res, next) => {
+const auth = (req:any, res:any, next:any) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
